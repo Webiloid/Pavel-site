f6c9f1819f2a3b027c1c3c587038cae4137189f4
Webiloid <projectwebcode@gmail.com>
2017-03-12 08:42:52 +0300

    slider-change

--- js/jquery.slider.js
+++ js/jquery.slider.js
@@ -1,18 +1,29 @@
 
 /*  Jquery slider plugin */
 
-(function($) {
+(function(global, $) {
 
   $.fn.slider = function(options) {
 
     // Дефолтные настройки
     let settings = {
       slideSelector: null,
-      controllerSelector: null
+      controllerSelector: null,
+      animationDuration: 300,
+      animationType: "scroll",
+      cyclical: false,
+      autoMode: false,
+      interval: 5000,
+      direction: "left"
     };
 
     settings = $.extend({}, settings, options);
-    let slides = this.find(settings.slideSelector);
+    let slides = this.find(".slider__slide");
+    let controllers = {
+      prev: this.find(".slider__controller--prev"),
+      next: this.find(".slider__controller--next")
+    };
+
 
     // Текущее состояние слайдера
     let state = {
@@ -24,29 +35,49 @@
     let initialize = function() {
 
       // TODO Добавить css-файл со свойствами слайда (класс slider + подклассы)
-      slides.not(`:eq(${state.currentSlide})`).addClass("slider__slide--invisible");
-      slides.eq(state.currentSlide).addClass("slider__slide");
+      slides.hide();
+      slides.eq(state.currentSlide).show();
 
       // TODO добавить селекторы для контроллеров (кнопок)
       // TODO обработать нажатие на контроллеры (+ кнопки + скролл мышкой)
+      controllers.prev.on("click", changeSlide.bind(global, false));
+      controllers.next.on("click", changeSlide.bind(global, true));
+
+      $(global).on("keydown", function(e) {
+        e.preventDefault();
+        let keyCode = e.keyCode;
+        if(keyCode === 38 || keyCode == 37) {
+          changeSlide(false);
+        } else if(keyCode == 40 || keyCode == 39) {
+          changeSlide(true);
+        }
+      });
+
+      $(global).on("scroll", function() {
+        console.log("scroll");
+      });
 
     };
 
-    // TODO переключение на следующий слайд
-    let next = function() {
+    let changeSlide = function(direction) {
 
-    };
+      slides.eq(state.currentSlide).hide();
 
-    // TODO переключение на пердыдущий слайд
-    let prev = function() {
+      if((state.currentSlide < state.slidesCount - 1) && direction) {
+        state.currentSlide += 1;
+      } else if(state.currentSlide > 0 && !direction) {
+        state.currentSlide -= 1;
+      }
 
+      slides.eq(state.currentSlide).fadeIn(settings.animationDuration);
     };
 
     // TODO сброс слайдера
     let reset = function() {
-
+      
     };
 
+    initialize();
   };
 
-})(jQuery);
+})(this, jQuery);

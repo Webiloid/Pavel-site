ff690a590093c15fb9a2ec0fd9e787233d6d567e
Webiloid <projectwebcode@gmail.com>
2017-03-20 00:03:34 +0300

    +search

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(".slider").slider({
 });
 
 $(window).on("load", function() {
+
   let circle = new ProgressBar.Circle("#progressbar", {
     strokeWidth: 6,
     easing: 'easeInOut',
@@ -19,5 +20,26 @@ $(window).on("load", function() {
   circle.animate(1.0, function() {
     $("#progressbar").hide();
     $(".slider__progressbar").fadeOut(800);
+  });
+
+  $(".navigation__item--search").click(function() {
+    $(".search__field").addClass("appear__input");
+    $(".search").css({
+      'z-index': 10000
+    }).animate({
+      opacity: 1
+    }, 300);
+  });
+
+  $(".search__close").click(function() {
+    $(".search__field").removeClass("appear__input").addClass("disappear__input");
+    $(".search").animate({
+      opacity: 0
+    }, 300, function() {
+      $(this).css({
+        'z-index': -1
+      });
+      $(".search__field").removeClass("disappear__input");
+    });
   });
 });
